2bbf763c6824c87f726566b3a1148910116bb1ea
mu <16834+mura303@users.noreply.github.com>
2024-12-14 15:42:07 +0900

    Update raw_youtube_player.dart

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -202,7 +202,7 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
             ..addJavaScriptHandler(
               handlerName: 'VideoTime',
               callback: (args) {
-                final position = args.first * 1000;
+                final position = (args.first ?? 0) * 1000;
                 final num buffered = args.last ?? 0;
                 controller!.updateValue(
                   controller!.value.copyWith(
